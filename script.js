374f17ce1632bd6cdaf7d990f3344e07d842eb6b
FelipeEnne <felipeenne@gmail.com>
2023-04-03 07:23:18 -0300

    finish

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
+const word = document.querySelector(".input-text");
 const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
 
-btn.addEventListener("click", palindrome);
+btn.addEventListener("click", countVowel);
 
-function palindrome() {
-  const word = document.querySelector(".input-text").value;
+function countVowel() {
+  let vowelCount = 0;
+  let wordVal = word.value.toLowerCase();
 
-  let len = word.length;
-
-  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
-  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
-
-  //let flip = end.split("").reverse().join("");
-  let flip = [...end].reverse().join("");
-
-  if (start === flip) {
-    result.innerHTML = `${word.toUpperCase()} is a palindrome`;
-  } else {
-    result.innerHTML = `${word.toUpperCase()} is NOT a palindrome`;
+  for (let i = 0; i < wordVal.length; i++) {
+    const letter = wordVal[i];
+    if (letter.match(/([a,e,i,o,u])/)) {
+      vowelCount++;
+    }
   }
+  result.innerHTML = `${word.value} has ${vowelCount} vowels`;
 }
